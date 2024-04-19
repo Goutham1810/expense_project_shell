2cfb2ed8a43eb6f5860ddd9b3424e3125034eb1f
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-19 14:03:52 +0530

    Database Script

--- mysql-install.sh
+++ mysql-install.sh
@@ -3,10 +3,10 @@
 #We have to do two validations one is executing the node with root user
 
 USERID=$(id -u)
-R=\e[31m
-G=\e[32m
-Y=\e[33m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
