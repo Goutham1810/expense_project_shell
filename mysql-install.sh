e7840055d17deef021de9e64688099c005cf6454
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-19 14:00:50 +0530

    Database Script

--- mysql-install.sh
+++ mysql-install.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+#We have to do two validations one is executing the node with root user
+
+USERID=$(id -u)
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+
+
+if [ USERID -ne 0 ]
+then
+    echo -e "Please Connect To $R SUDO USER $N"
+else
+    echo -e "Connected To $G SUDO USER $N"
+fi
