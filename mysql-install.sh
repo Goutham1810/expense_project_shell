d3a3b0e40ab5d5c50130b1d7e388091d12da11aa
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-20 16:09:42 +0530

    Mysql Program Update

--- mysql-install.sh
+++ mysql-install.sh
@@ -39,5 +39,18 @@ VALIDATE $? "Enabling mysqld service"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting mysqld service"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
-VALIDATE $? "Setting up root password"
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+#VALIDATE $? "Setting up root password"
+
+# In order to handle the Idempotent of shell script to avoid executing multiple times of mysql server password
+# set connectivity 
+
+mysql -h db.somustack.online -uroot -pExpenseApp@1 -e "SHOW DATABASES" &>>$LOGFILE
+
+if [ $? -eq 0 ]
+then
+    echo -e "The ROOT Password $Y ALREADY CONFIGURED $G NO ACTION TO BE PERFORMED $N "
+else
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+    VALIDATE $? "Setting Up $Y ROOT PASSWORD $N"
+fi
