cca9aa6616463fb9541888bac953f5544e28e604
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-20 22:45:57 +0530

    Mysql Program Update

--- backend-install.sh
+++ backend-install.sh
@@ -10,7 +10,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
+echo "Please Enter Password:"
+read -s mysql_root_password
 if [ $USERID -ne 0 ]
 then
     echo -e "Please Connect To $R SUDO USER $N"
@@ -39,12 +40,50 @@ VALIDATE $? "Enabling NodeJs 20 Version "
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing NodeJS"
 
-id expense
+id expense &>>$LOGFILE
+
 if [ $? -eq 0 ]
 then
-    echo "The UserID Already $G Created..$N"
+    echo -e "The UserID Already $G Created..$N"
     exit 1;
 else
-    useradd expense
+    useradd expense &>>$LOGFILE
     VALIDATE $? "Creating UserID"
 fi
+
+mkdir /app &>>$LOGFILE
+VALIDATE $? "Moving To APP Directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "Download Artifactory"
+
+cd /app &>>$LOGFILE
+rm -rf /app/* &>>$LOGFILE
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracting the Code"
+
+cd /app
+npm install &>>$LOGFILE
+VALIDATE $? "NodeJS Installation"
+
+#check your repo and path
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copy backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon Reload"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Starting backend"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting Backend"
